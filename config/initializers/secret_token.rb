586e186cae39f631b53a331b69184c32fa45e3bc
suresh <thesuresh14@gmail.com>
2011-07-13 14:50:15 +0900

    Initial commit 5

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pahilo::Application.config.secret_token = '68767323561a640dbc20fed1aba7695dd642d90664e8d2337e69a703c1ec5eda41edbcb4a64bae6b9e484e4b7c347d7b1bcc8ebf36bf880fb683bac7d671f647'
